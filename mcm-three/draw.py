5530f7d5c4363715fe2c7b114ed988943635ca65
ariescc <jiachang@mail.nwpu.edu.cn>
2018-02-05 11:48:18 +0800

    汉译英

--- mcm-three/draw.py
+++ mcm-three/draw.py
@@ -7,7 +7,7 @@ plt.rcParams['font.sans-serif'] = ['SimHei']
 plt.rcParams['font.family'] = 'sans-serif'
 plt.rcParams['axes.unicode_minus'] = False
 
-X = ['阿根廷', '亚美尼亚', '澳大利亚', '阿塞拜疆', '比利时', '保加利亚', '白俄罗斯', '巴西', '博茨瓦纳', '加拿大']
+X = ['Argentina', 'Armenia', 'Australia', 'azerbaijan', 'Belgium', 'Bulgaria', 'belarus', 'Brazil', 'Botswana', 'Canada']
 Y = [0.34907581451644099, 0.28976857860299399, 0.57396702931141019, 0.41924887132432448, 0.53632288155511376, 0.36975553614783396, 0.41938632529574055, 0.4987781460848873, 0.24604314628276353, 0.65176053910203235]
 # 设置字体倾斜
 plt.xticks(rotation=45)
