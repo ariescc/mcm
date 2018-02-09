79fe2f073c323adff0618be0b7493cbd02c788f0
ariescc <jiachang@mail.nwpu.edu.cn>
2018-02-09 21:48:35 +0800

    数据字典

---
+++ icm/zbdict.py
@@ -0,0 +1,15 @@
+index_dict = {
+    'SA': 'Security Apparatus',
+    'FE': 'Factionalized Elites',
+    'GG': 'Group Grievances',
+    'EC': 'Economic Decline',
+    'UD': 'Uneven Economic Development',
+    'HF': 'Human Flight And Brain Drain',
+    'SL': 'State Legitimary',
+    'PS': 'Public Services',
+    'HR': 'Human Rights And Rule Of Law',
+    'OP': 'Demographsc Pressures',
+    'RO': 'Refugees and IDPs',
+    'EX': 'External Intervention'
+}
+
